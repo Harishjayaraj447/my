b60b853f5beaa3838f315b675578685fedc535ed
Harishjayaraj447 <89262824+Harishjayaraj447@users.noreply.github.com>
2021-08-21 03:25:57 +0530

    Update pppy.py

--- pppy.py
+++ pppy.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-##########################################
 # Duino-Coin Python PC Miner (v2.6.1)
 # https://github.com/revoxhere/duino-coin
 # Distributed under MIT license
 # © Duino-Coin Community 2019-2021
-##########################################
 # Import libraries
 import sys
 from configparser import ConfigParser
